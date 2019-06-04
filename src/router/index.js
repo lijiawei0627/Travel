7e7014c382291ff048c9eb76f7045ea79f47fe76
ljw <1472406718@qq.com>
2019-06-04 14:45:11 +0800

    路由优化

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../pages/home/Home'
 
 Vue.use(Router)
 
@@ -9,7 +8,7 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: () => import('@/pages/home/Home')
     },
     {
       path: '/city',
